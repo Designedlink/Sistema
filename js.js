3d4675c486634d217339c3d485047b589cd1a1fd
Designedlink <79681687+Designedlink@users.noreply.github.com>
2022-06-11 23:51:20 -0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+var home = document.getElementById('home');
+var menu = document.getElementById('menu');
+
+function . activate();
+
+{
+    menu.classList.add("activo")
+}
+
+home.addEventListener("click", activar);
